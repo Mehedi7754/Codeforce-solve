e8a2a63c0112e0cb33d64538990abb1efe228085
Mehedi7754 <155046042+Mehedi7754@users.noreply.github.com>
2024-02-29 13:17:14 +0600

    Add files via upload

---
+++ codeforce 69A.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,x,y,z,i,s1=0,s2=0,s3=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d%d",&x,&y,&z);
+        s1=s1+x;
+        s2=s2+y;
+        s3=s3+z;
+    }
+    if(s1==0&&s2==0&&s3==0)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
+
